5e6efebef7e8f7cde5ed12eb34a1db1c1e78c7b9
yatinsingh2007 <yatin4591@gmail.com>
2026-03-17 11:00:37 +0530

    feat: Add error handling for reading user input.

--- 03userInput/main.go
+++ 03userInput/main.go
@@ -11,7 +11,10 @@ func main() {
 	fmt.Println("Enter your rating of pizza :")
 	reader := bufio.NewReader(os.Stdin)
 	// comma ok , comma error
-	input , _ := reader.ReadString('\n')
+	input , err := reader.ReadString('\n');
+	if err != nil{
+		fmt.Println(err)
+	}
 	fmt.Printf("The rating of your pizza is : %s\n", input)
 	fmt.Printf("The type of rating of your pizza is : %T\n", input)
 }
